f8029200e9ca4770c7210376fb3544818ec460db
Mehdi Avdi <mehdi.avdi@gmail.com>
2015-01-02 18:30:24 +0000

    comments for protractor config file

--- test/config.js
+++ test/config.js
@@ -1,22 +1,30 @@
 exports.config = {
+  //this is where our selenium server by default sits
   seleniumAddress: 'http://localhost:4444/wd/hub',
 
+  //This is the framework we use to write our tests, in this case cucumber
   framework: 'cucumber',
 
-  // Spec patterns are relative to this directory.
+  // Spec patterns are relative to this directory
   specs: [
     'features/**/*'
   ],
 
+  //We ask protractor to run our tests in chrome
   capabilities: {
     'browserName': 'chrome'
   },
 
-  baseUrl: 'http://localhost:3000',
+  //We tell protractor it can access our app in this location
+  baseUrl: 'http://localhost:8000',
 
+  //cucumber options
   cucumberOpts: {
+    //find the step definitions here
     require: 'features/step_definitions/**/*.js',
+    //this outputs better information
     format: 'pretty',
+    //ask tests to wait 20 seconds before they timeout
     timeout: 20000
   }
 };
